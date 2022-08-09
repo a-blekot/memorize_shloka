1dd426599c59561cd9e7a07e7d0936194128ae59
a-blekot <aleksey.blekot@gmail.com>
2022-08-09 11:55:45 +0300

    increase build number

--- build-logic/convention-plugins/src/main/kotlin/ApkConfig.kt
+++ build-logic/convention-plugins/src/main/kotlin/ApkConfig.kt
@@ -8,8 +8,8 @@ object ApkConfig {
     const val TARGET_SDK_VERSION = 32
     const val COMPILE_SDK_VERSION = 32
 
-    const val VERSION_CODE = 3
-    const val VERSION_NAME = "1.2"
+    const val VERSION_CODE = 4
+    const val VERSION_NAME = "1.3"
 
     val JAVA_VERSION = JavaVersion.VERSION_1_8
     val JAVA_VERSION_NAME = JavaVersion.VERSION_1_8.toString()
