c5349a8d380da38c4238baec5e5d1e06f33edbe9
a-blekot <aleksey.blekot@gmail.com>
2022-09-23 12:55:08 +0300

    update version 14 (1.6.1)

--- build-logic/convention-plugins/src/main/kotlin/ApkConfig.kt
+++ build-logic/convention-plugins/src/main/kotlin/ApkConfig.kt
@@ -8,8 +8,8 @@ object ApkConfig {
     const val TARGET_SDK_VERSION = 32
     const val COMPILE_SDK_VERSION = 32
 
-    const val VERSION_CODE = 13
-    const val VERSION_NAME = "1.6.0"
+    const val VERSION_CODE = 14
+    const val VERSION_NAME = "1.6.1"
 
     val JAVA_VERSION = JavaVersion.VERSION_1_8
     val JAVA_VERSION_NAME = JavaVersion.VERSION_1_8.toString()
