c9106d0fbcc5071e94ecc6232dd3a08f22f21847
a-blekot <aleksey.blekot@gmail.com>
2023-05-12 14:45:22 +0300

    version 1.9.1 (19)

--- build-logic/convention-plugins/src/main/kotlin/ApkConfig.kt
+++ build-logic/convention-plugins/src/main/kotlin/ApkConfig.kt
@@ -8,8 +8,8 @@ object ApkConfig {
     const val TARGET_SDK_VERSION = 33
     const val COMPILE_SDK_VERSION = 33
 
-    const val VERSION_CODE = 18
-    const val VERSION_NAME = "1.9"
+    const val VERSION_CODE = 19
+    const val VERSION_NAME = "1.9.1"
 
     val JAVA_VERSION = JavaVersion.VERSION_1_8
     val JAVA_VERSION_NAME = JavaVersion.VERSION_1_8.toString()
